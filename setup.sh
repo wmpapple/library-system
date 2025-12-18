ce4157d23246ebaec42bd040646266e9a80ef52b
美蘋 <2482970191@qq.com>
2025-12-18 15:11:03 +0800

    set更新

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 pip install -r .\requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple --trusted-host pypi.tuna.tsinghua.edu.cn
 npm install @vue/composition-api --save-dev
 
+docker-compose up -d
 # frontend
 conda activate dbs
 cd frontend
@@ -10,3 +11,7 @@ npm run serve
 # backend
 conda activate dbs
 uvicorn backend.app.main:app --reload
+
+docker exec -it library_backend /bin/bash
+ls app
+python -m app.database
